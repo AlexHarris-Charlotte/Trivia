62d3754e4f7f40d006fb62dbbaf3a332f7867518
Alex Harris <akharri8@ncsu.edu>
2018-01-14 11:10:55 -0500

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,10 +18,13 @@ window.onload = function() {
     var startButton = document.createElement("button");
     startButton.setAttribute("id", "start");
     var buttonText = document.createTextNode("Start");
-    var div1 = document.getElementById("container");
+    var div1 = document.createElement("div");
+    body.appendChild(div1);
+    var div2 = document.getElementById("container");
     var newDiv = document.createElement("div");
     var timeP = document.createElement("p");
     var questionP = document.createElement("p");
+    var image = document.createElement("img");
 
     function questionObject(question, answers, correctAnswer, gif) {
         this.question = question;
@@ -32,56 +35,99 @@ window.onload = function() {
 
     var Russel = new questionObject("What player has won the most NBA titles in NBA History?", ["Michael Jordan", "Shaq", "Magic Johnson", "Bill Russel"], "Bill Russel", "assets/images/test.jpg");
     var Kareem = new questionObject("Who has scored the most points in NBA history?", ["Michael Jordan", "Kareem Abdul Jabar", "Lebron James", "Karl Malone"], "Kareem Abdul Jabar", "assets/images/test.jpg");
+    var Stock = new questionObject("Who has the most assists in NBA history?", ["Chris Paul", "Steve Nash", "John Stockton", "Magic Johnson"], "John Stockton", "assets/images/test.jpg");
+    //var Kareem = new questionObject("Who has scored the most points in NBA history?", ["Michael Jordan", "Kareem Abdul Jabar", "Lebron James", "Karl Malone"], "Kareem Abdul Jabar", "assets/images/test.jpg");
+    //var Kareem = new questionObject("Who has scored the most points in NBA history?", ["Michael Jordan", "Kareem Abdul Jabar", "Lebron James", "Karl Malone"], "Kareem Abdul Jabar", "assets/images/test.jpg");
+    //var Kareem = new questionObject("Who has scored the most points in NBA history?", ["Michael Jordan", "Kareem Abdul Jabar", "Lebron James", "Karl Malone"], "Kareem Abdul Jabar", "assets/images/test.jpg");
+    //var Kareem = new questionObject("Who has scored the most points in NBA history?", ["Michael Jordan", "Kareem Abdul Jabar", "Lebron James", "Karl Malone"], "Kareem Abdul Jabar", "assets/images/test.jpg");
+    //var Kareem = new questionObject("Who has scored the most points in NBA history?", ["Michael Jordan", "Kareem Abdul Jabar", "Lebron James", "Karl Malone"], "Kareem Abdul Jabar", "assets/images/test.jpg");
 
 
     var objectArray = [Russel, Kareem];
 
 
-    // Creates Button
     startButton.appendChild(buttonText);
     document.body.appendChild(startButton);
 
 
     startButton.addEventListener("click", function() {
+        var timeCount = 30;
+        var timeCountP = document.createElement("p");
+        div1.appendChild(timeCountP);
         var strValue;
         var answerButton;
-        // need timer to decrement using SetInterval
         body.removeChild(startButton);
         nextQuestion();
+        // Prob make this interval into a function
+        // setInterval(function(){ 
+        //     if(timeCount > 0) {
+        //         timeCountP.textContent = timeCount;
+        //         timeCount--;
+        //     } else {
+        //         timeCount = 30;
+        //         nextQuestion();
+        //         // need to create a function that shows the gif and correct answer. this function
+        //         // needs a callback to nextquestion
+        //     }
+        // }, 1000)
     })
 
     function nextQuestion() {
         var index = 0;
         questionText = document.createTextNode(objectArray[index].question);
         questionP.appendChild(questionText);
-        div1.appendChild(questionP);
+        div2.appendChild(questionP);
         for(var i = 0; i < objectArray[index].answers.length; i++) {
             let buttonText = document.createTextNode(objectArray[index].answers[i]);
             answerButton = document.createElement("button");
             answerButton.className += "answers";
-            // strValue = objectArray[index].answers[i].toString();
             if(objectArray[index].answers[i] === objectArray[index].correctAnswer) {
                 answerButton.setAttribute("id", "correctAnswer");
                 console.log(answerButton);
             }
-            addEventStuff(answerButton);
-            // className = document.getElementsByClassName("answers");
-            
             answerButton.appendChild(buttonText);
-            div1.appendChild(answerButton);
+            div2.appendChild(answerButton);
+            addEventStuff(answerButton);
         }
 
         function addEventStuff(answerButton){ 
+            var response;
             answerButton.addEventListener("click", function() {
-                if(this.getAttribute("id") === "correctAnswer"){    // Logic works
-                    console.log(this)
-                    console.log("got the correct answer");
-                } else {
-                    console.log(this)
-                    console.log("not the correct answer");
+                if(this.getAttribute("id") === "correctAnswer"){    
+                    // Need to increment an answer score to show player at end of game
+                    response = "Correct! ";
+                    questionResponse(response);
+                } else if(this.getAttribute("id") !== "correctAnswer") {
+                    // Need to increment an answer score to show player at end of game
+                    response = "Incorrect! ";
+                    questionResponse(response);
                 } 
             });
         }
+
+        function questionResponse(response) {
+            var responseText = document.createElement("p");
+            removeElements();
+            image.setAttribute("src", objectArray[index].gif);
+            image.style.width = "300px";
+            image.style.height = "300px";
+            div2.appendChild(image);
+            responseText.textContent = response + "The answer was " + objectArray[index].correctAnswer + "!";
+            div2.appendChild(responseText);
+            // index++; isnt seen by original index
+            setTimeout(function(){
+                removeElements();
+                nextQuestion();
+                // make call to nextQuestion here
+            },1000 * 8)
+        } 
+        function removeElements() {
+            while (div2.firstChild) { // works
+                div2.removeChild(div2.firstChild);
+            }
+        }
+        index++;
+        console.log(index);
     }
 
 
